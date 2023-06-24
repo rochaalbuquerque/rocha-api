a9c40607bd4edab224838868ede6974f5683260e
rochaalbuquerque <alexrochadealbuquerque@gmail.com>
2023-06-24 16:04:44 -0300

    projeto criado

---
+++ src/main/java/com/br/rocha/RochaApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.rocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RochaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RochaApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/br/rocha/config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.br.rocha.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.expression.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
+		return true;
+	}
+
+}

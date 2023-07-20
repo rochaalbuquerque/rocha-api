98b4246eb972ccb07f8e0a4feb3966daf9a1a77d
rochaalbuquerque <alexrochadealbuquerque@gmail.com>
2023-07-20 11:07:57 -0300

    configuracao do swagger 3

--- src/main/java/com/br/rocha/resources/HelloWorld.java
+++ src/main/java/com/br/rocha/resources/HelloWorld.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloWorld {
 
 	@GetMapping
+	@Operation(summary = "Bem vindos!")
 	public String helloWorld() {
 		return "Hello World! This is the Rocha base project";
 	}

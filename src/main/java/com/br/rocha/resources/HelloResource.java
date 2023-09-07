8ede28c731744751306fd685e7a4f57204dfd482
rochaalbuquerque <alexrochadealbuquerque@gmail.com>
2023-09-07 18:42:52 -0300

    add ON no texto do endPoint hello

--- src/main/java/com/br/rocha/resources/HelloResource.java
+++ src/main/java/com/br/rocha/resources/HelloResource.java
@@ -13,7 +13,7 @@ public class HelloResource {
 	@GetMapping
 	@Operation(summary = "Bem vindos!")
 	public String helloWorld() {
-		return "Hello World! This is the Rocha base project";
+		return "Hello World! This is the Rocha base project ON";
 	}
 
 }

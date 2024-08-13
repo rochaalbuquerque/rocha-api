e54c96ee1cf01c0ae9c8d39519b4ff064929c474
Alex Rocha <alexrochadealbuquerque@gmail.com>
2024-08-13 15:13:25 -0300

    [develop] update - Ajuste Client, WebConfig CORSS e outros

--- src/main/java/com/br/rocha/dto/ResponseClientDTO.java
+++ src/main/java/com/br/rocha/dto/ResponseClientDTO.java
@@ -11,7 +11,7 @@ public class ResponseClientDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-	private String fistName;
+	private String firstName;
 	private String lastName;
 
 }
